8df13ca970b272b045cc79373d59c3144031bcc4
mareikeherzog <herzmare@gmail.com>
2017-10-20 08:34:56 +0100

    Update gt-filter-lax.pl
    Added comments to aid understanding of users.

--- gt-filter-lax.pl
+++ gt-filter-lax.pl
@@ -1,16 +1,17 @@
 # These example shows that the VCF output line can be edited. (Thanks to Shane McCarthy)
+# The filter threshold can be edited where indicated by the comments 
 
 {
     tag      => 'FORMAT/GQ',
     name     => 'MinSampleGQ',
-    desc     => 'Genotypes set to . for samples with GQ < 10', 
+    desc     => 'Genotypes set to . for samples with GQ < 10', #Edit Threshold here
     apply_to => 'all',
     test     => sub {
         my $i = 8;
         for my $gq (@$MATCH)
         {
             $i++;
-            next unless ($gq<10); 
+            next unless ($gq<10); #Edit Threshold here
             my @format = split(/:/,$$RECORD[$i]);
             $format[0] = $format[0] =~ /\// ? "./." : ".";
             $$RECORD[$i] = join(":",@format);
